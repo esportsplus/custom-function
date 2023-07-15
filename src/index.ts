e35ec2bcd2f8f11c4ab7a977358903ad0d498a75
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-07-14 23:35:12 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let { setPrototypeOf } = Object;
+
+
+const CustomFunction = (function <T extends Function>(fn: T): T {
+    return setPrototypeOf(fn, new.target.prototype);
+});
+
+CustomFunction.prototype = Function.prototype;
+
+
+export default CustomFunction as {
+    new(...args: Parameters<typeof CustomFunction>): typeof CustomFunction;
+    (...args: Parameters<typeof CustomFunction>): typeof CustomFunction;
+};
